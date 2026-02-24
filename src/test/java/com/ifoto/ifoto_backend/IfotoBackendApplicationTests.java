cc809c8c8061073742404bd1eb20ae413ef5d956
Lio Kock Hock <liokockhock@gmail.com>
2026-02-24 15:03:38 +0800

    fix: use correct profile flag for CI maven test

--- src/test/java/com/ifoto/ifoto_backend/IfotoBackendApplicationTests.java
+++ src/test/java/com/ifoto/ifoto_backend/IfotoBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.ifoto.ifoto_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IfotoBackendApplicationTests {
 
 	@Test
