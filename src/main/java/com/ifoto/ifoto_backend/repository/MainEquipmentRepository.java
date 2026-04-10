e409a40a5ae905fa99a488dcd4405dbc810ee44c
Lio Kock Hock <liokockhock@gmail.com>
2026-04-10 10:27:41 +0800

    fix: performance issue

--- src/main/java/com/ifoto/ifoto_backend/repository/MainEquipmentRepository.java
+++ src/main/java/com/ifoto/ifoto_backend/repository/MainEquipmentRepository.java
@@ -2,6 +2,12 @@ package com.ifoto.ifoto_backend.repository;
 
 import com.ifoto.ifoto_backend.model.MainEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MainEquipmentRepository extends JpaRepository<MainEquipment, Long> {
+
+    @Modifying
+    @Query("DELETE FROM MainEquipment e WHERE e.mainEquipmentId = :id")
+    int deleteByIdReturningCount(Long id);
 }
--- src/main/java/com/ifoto/ifoto_backend/repository/SubEquipmentRepository.java
+++ src/main/java/com/ifoto/ifoto_backend/repository/SubEquipmentRepository.java
@@ -2,6 +2,12 @@ package com.ifoto.ifoto_backend.repository;
 
 import com.ifoto.ifoto_backend.model.SubEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubEquipmentRepository extends JpaRepository<SubEquipment, Long> {
+
+    @Modifying
+    @Query("DELETE FROM SubEquipment e WHERE e.subEquipmentId = :id")
+    int deleteByIdReturningCount(Long id);
 }
