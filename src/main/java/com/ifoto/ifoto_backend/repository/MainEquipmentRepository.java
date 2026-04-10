2e03e644acaed1815fb12db4806aa85a958d9c85
Lio Kock Hock <liokockhock@gmail.com>
2026-04-10 10:31:01 +0800

    fix: bug risk

--- src/main/java/com/ifoto/ifoto_backend/repository/MainEquipmentRepository.java
+++ src/main/java/com/ifoto/ifoto_backend/repository/MainEquipmentRepository.java
@@ -4,10 +4,11 @@ import com.ifoto.ifoto_backend.model.MainEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MainEquipmentRepository extends JpaRepository<MainEquipment, Long> {
 
     @Modifying
     @Query("DELETE FROM MainEquipment e WHERE e.mainEquipmentId = :id")
-    int deleteByIdReturningCount(Long id);
+    int deleteByIdReturningCount(@Param("id") Long id);
 }
--- src/main/java/com/ifoto/ifoto_backend/repository/SubEquipmentRepository.java
+++ src/main/java/com/ifoto/ifoto_backend/repository/SubEquipmentRepository.java
@@ -4,10 +4,11 @@ import com.ifoto.ifoto_backend.model.SubEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubEquipmentRepository extends JpaRepository<SubEquipment, Long> {
 
     @Modifying
     @Query("DELETE FROM SubEquipment e WHERE e.subEquipmentId = :id")
-    int deleteByIdReturningCount(Long id);
+    int deleteByIdReturningCount(@Param("id") Long id);
 }
